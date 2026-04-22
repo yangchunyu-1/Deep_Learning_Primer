6d0e137b5d1b7382e9f81b3495028014165a960e
Yang <178874713@qq.com>
2026-04-22 21:34:56 +0800

    Softmax函数

---
+++ softmax.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y =exp_a / sum_exp_a
+    return y
+
+#优化溢出问题：
+def softmax_new(a):
+    c =np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
